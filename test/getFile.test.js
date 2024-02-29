7c5a33c4f67fe0ce2622d5d1ace69ac8123c0fa0
ZhytnkovskaIryna <iryna.zhytnikovska@gmail.com>
2024-02-29 15:01:15 +0200

    upload added

--- test/getFile.test.js
+++ test/getFile.test.js
@@ -1,14 +1,52 @@
 const axios = require("axios");
 const expect = require("chai").expect;
+const token =
+  "Bearer " +
+  "sl.BwgySG-EbDFjmzkH2w6-5UFgA-4WgaWY8Xfo5SCmGltXctb6bolhgsWbKtii_DwPNsviS-hHGRCAmC1QjA37w6VS4Jd5FD0GkDxyLfWctmpYon3jJIgOlyocH9mdcZXMvi6YEAoHqtCWx4MFBC2t6Sk";
 
 describe("Get file count", async () => {
   it("Test1", async () => {
     const response = await axios.post(
       "https://api.dropboxapi.com/2/file_requests/count",
+      null,
       {
         headers: {
-          Authorization:
-            "Bearer sl.Bwfm1wBKbDNixxQz_uPbIGWWOnBvYALULhTqhwNMZIresWeyLY8HaWDciv98y-UuUXxnYuHHXCPkrkafh9rx9m2-MVh-xpOascf-ZGAWg1GJn0b0ELuZ_Fs9CkodxUO_MT2v2lNEKqa3plt5cVPcdiU",
+          Authorization: token,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log(response.data);
+    expect(response.status).equals(200); //asserting if the response code is 200
+  });
+  /*
+  it("Test2", async () => {
+    const response = await axios.post(
+      "https://api.dropboxapi.com/2/files/delete_v2",
+      {
+        path: "/package.json",
+      },
+      {
+        headers: {
+          Authorization: token,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log(response.data);
+    expect(response.status).equals(200); //asserting if the response code is 200
+  });
+*/
+  it("Upload file", async () => {
+    const response = await axios.post(
+      "https://content.dropboxapi.com/2/files/upload",
+      "@file1.txt",
+      {
+        headers: {
+          Authorization: token,
+          "Content-Type": "application/octet-stream",
+          "Dropbox-API-Arg":
+            '{"autorename": false,"mode": "add","mute": false,"path": "/file1.txt","strict_conflict": false }',
         },
       }
     );
