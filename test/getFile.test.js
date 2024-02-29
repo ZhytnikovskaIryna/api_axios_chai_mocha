765018c29d4b1cb3b1d12e1e5474b7aff30eab0f
ZhytnkovskaIryna <iryna.zhytnikovska@gmail.com>
2024-02-29 15:45:26 +0200

    all tests added

--- test/getFile.test.js
+++ test/getFile.test.js
@@ -2,29 +2,33 @@ const axios = require("axios");
 const expect = require("chai").expect;
 const token =
   "Bearer " +
-  "sl.BwgySG-EbDFjmzkH2w6-5UFgA-4WgaWY8Xfo5SCmGltXctb6bolhgsWbKtii_DwPNsviS-hHGRCAmC1QjA37w6VS4Jd5FD0GkDxyLfWctmpYon3jJIgOlyocH9mdcZXMvi6YEAoHqtCWx4MFBC2t6Sk";
+  "sl.BwgOe-rQOxBGOBkUlcBqeo_CdPWYrjJSpdQAtaklG42hTbtWnvZyO7i5GAay1tBSdJ8ZyKxVVEns6UFDTj38q0lSF0wB--pAF495hlQrK3Hw2iJEBKOYBnA3_JbBlMBy6NtG-cJM85wUPucfqDnOn74";
 
-describe("Get file count", async () => {
-  it("Test1", async () => {
+describe("Dropbox basic api testing", async () => {
+  it("Upload file", async () => {
     const response = await axios.post(
-      "https://api.dropboxapi.com/2/file_requests/count",
-      null,
+      "https://content.dropboxapi.com/2/files/upload",
+      "@file1.txt",
       {
         headers: {
           Authorization: token,
-          "Content-Type": "application/json",
+          "Content-Type": "application/octet-stream",
+          "Dropbox-API-Arg":
+            '{"autorename": false,"mode": "add","mute": false,"path": "/file1.txt","strict_conflict": false }',
         },
       }
     );
     console.log(response.data);
     expect(response.status).equals(200); //asserting if the response code is 200
   });
-  /*
-  it("Test2", async () => {
+  it("Get file metadata", async () => {
     const response = await axios.post(
-      "https://api.dropboxapi.com/2/files/delete_v2",
+      "https://api.dropboxapi.com/2/files/get_metadata",
       {
-        path: "/package.json",
+        include_deleted: false,
+        include_has_explicit_shared_members: false,
+        include_media_info: false,
+        path: "id:UGCBj4DAw-oAAAAAAAAACw",
       },
       {
         headers: {
@@ -36,17 +40,17 @@ describe("Get file count", async () => {
     console.log(response.data);
     expect(response.status).equals(200); //asserting if the response code is 200
   });
-*/
-  it("Upload file", async () => {
+
+  it("Delete file", async () => {
     const response = await axios.post(
-      "https://content.dropboxapi.com/2/files/upload",
-      "@file1.txt",
+      "https://api.dropboxapi.com/2/files/delete_v2",
+      {
+        path: "/file1.txt",
+      },
       {
         headers: {
           Authorization: token,
-          "Content-Type": "application/octet-stream",
-          "Dropbox-API-Arg":
-            '{"autorename": false,"mode": "add","mute": false,"path": "/file1.txt","strict_conflict": false }',
+          "Content-Type": "application/json",
         },
       }
     );
